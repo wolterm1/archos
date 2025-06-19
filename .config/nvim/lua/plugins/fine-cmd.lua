01e87d2db3ed20e48dd70ed4349c53ab98e9e932
wolterm1 <woltermatthias199@gmail.com>
2025-06-19 23:21:26 +0200

    neovim backup

--- .config/nvim/lua/plugins/fine-cmd.lua
+++
@@ -1,7 +0,0 @@
-return {
-  'MunifTanjim/nui.nvim',
-  dependencies = {
-    'VonHeikemen/fine-cmdline.nvim',
-  },
-  vim.api.nvim_set_keymap('n', ':', '<cmd>FineCmdline<CR>', { noremap = true }),
-}
